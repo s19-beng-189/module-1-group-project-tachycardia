53c0e498bb84c4696ea5851c84677cbfe4dc7a30
Schmolesky <49256768+Schmolesky@users.noreply.github.com>
2019-04-28 15:35:29 -0700

    changed parameter values to normal
    made same changes from module 1.2 to make pressure 120/80

--- Model 2/in_LV_sa.m
+++ Model 2/in_LV_sa.m
@@ -1,31 +1,32 @@
 %filename: in_LV_sa.m  (initialization for LV_sa)
-T =0.0125    %Duration of heartbeat (minutes)
-TS=0.0050    %Duration of systole   (minutes)
-tauS=0.0025  %CLV time constant during systole (minutes)
-tauD=0.0075  %CLV time constant during diastole (minutes)
-Rs=17.86     %Systemic resistance (mmHg/(liter/minute))
+T =0.0125;    %Duration of heartbeat (minutes)
+TS=0.0050;    %Duration of systole   (minutes)
+tauS=0.0025;  %CLV time constant during systole (minutes)
+tauD=0.0075;  %CLV time constant during diastole (minutes)
+Rs=17.86+1.9;     %Systemic resistance (mmHg/(liter/minute))
 %Valve resistances here are not supposed to be realistic,
 %just small enough to be negligible:
-RMi=0.01     %mitral valve resistance (mmHg/(liter/minute))
-RAo=0.01     %aortic valve resistance (mmHg/(liter/minute))
+RMi=0.01;     %mitral valve resistance (mmHg/(liter/minute))
+RAo=0.01;     %aortic valve resistance (mmHg/(liter/minute))
 %The following value of Csa is approximate;
 %needs adjustment to make blood pressure 120/80:
-Csa=0.00175  %Systemic arterial compliance (liters/mmHg)
-CLVS=0.00003 %Min (systolic)  value of CLV (liters/mmHg)
-CLVD=0.0146  %Max (diastolic) value of CLV (liters/mmHg)
-Vsad=0.825   %Systemic arterial volume when Psa=0 (liters)
-VLVd=0.027   %Left ventricular volume when PLV=0 (liters)
-PLA=5        %Left atrial pressure (mmHg)
-dt=0.01*T    %Time step duration (minutes)
+
+Csa=0.00175*127/192;  %Systemic arterial compliance (liters/mmHg)
+CLVS=0.00003*4/3; %Min (systolic)  value of CLV (liters/mmHg)
+CLVD=0.0146;  %Max (diastolic) value of CLV (liters/mmHg)
+Vsad=0.825;   %Systemic arterial volume when Psa=0 (liters)
+VLVd=0.027;   %Left ventricular volume when PLV=0 (liters)
+PLA=5;        %Left atrial pressure (mmHg)
+dt=0.01*T;    %Time step duration (minutes)
 %This choice implies 100 timesteps per cardiac cycle.
-klokmax=15*T/dt %Total number of timesteps 
+klokmax=15*T/dt; %Total number of timesteps 
 %This choice implies simulation of 15 cardiac cycles.
-PLV=5                    %Initial value of PLV (mmHg)
-Psa=80                   %Initial value of Psa (mmHg)
+PLV=5;                    %Initial value of PLV (mmHg)
+Psa=80;                   %Initial value of Psa (mmHg)
 %set initial valve states:
 SMi=(PLA>PLV); %evaluates to 1 if PLA>PLV, 0 otherwise
 SAo=(PLV>Psa); %evaluates to 1 if PLV>Psa, 0 otherwise
-CLV=CV_now(0,CLVS,CLVD)  %Initial value of CLV (liters/mmHg)
+CLV=CV_now(0,CLVS,CLVD);  %Initial value of CLV (liters/mmHg)
 %Initialize arrays used to store data for plotting:
 %Although the program will work without doing this, 
 %it will run MUCH faster if MATLAB knows in advance 
@@ -43,4 +44,4 @@ SMi_plot=zeros(1,klokmax);
 SAo_plot=zeros(1,klokmax);
 %For self-checking in the function PLV_Psa_new, set CHECK=1                    
 %To skip the self-checking, set CHECK=0
-CHECK=1
+CHECK=0;
