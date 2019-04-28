30bda35805c7638e77e0d043cbbe8d2f539616f2
Schmolesky <49256768+Schmolesky@users.noreply.github.com>
2019-04-28 15:34:38 -0700

    Labeled graphs
    changed parameters in "in_LV"

--- Model 2/LV_sa.m
+++ Model 2/LV_sa.m
@@ -3,10 +3,6 @@ clear all %clear all variables
 clf       %and figures
 global T TS tauS tauD;
 global Csa Rs RMi RAo dt CHECK PLA;
-in_LV_sa  %initialize
-Csa=127*Csa/192; %decreased Csa
-Rs=Rs+1.9;   %increased Rs
-CLVS=CLVS*4/3;   %increased CLVS
 for klok=1:klokmax
   t=klok*dt;
   PLV_old=PLV;
