1030d993ea0f37654439f360d082f130d5c4d4a7
Schmolesky <49256768+Schmolesky@users.noreply.github.com>
2019-04-30 18:47:53 -0700

    changed PSA; changed graph names

--- Model 2/LV_sa.m
+++ Model 2/LV_sa.m
@@ -29,23 +29,23 @@ end
 %plot results:
 figure(1)
 subplot(3,1,1), plot(t_plot,CLV_plot)
-title('Left Ventricular Compliance');
+title('Left Ventricular Compliance: Exercise');
 subplot(3,1,2), plot(t_plot,PLV_plot,t_plot,Psa_plot)
 legend('PLV','Psa');
-title('Left ventricular pressure and systemic arterial pressure');
+title('Left ventricular pressure and systemic arterial pressure:exercise');
 subplot(3,1,3), plot(t_plot,QMi_plot,t_plot,QAo_plot,t_plot,Qs_plot)
-title('computer simulated pulsatile blood flow')
+title('computer simulated pulsatile blood flow:exercise')
 legend('Mitral valve Flow', 'Aortic Valve flow','systemic arterial flow')
 %left ventricular pressure-volume loop
 figure(2)
-plot(VLV_plot,PLV_plot)
-title('PV loop for left ventricle');
+plot(VLV_plot(1200:1500),PLV_plot(1200:1500))
+title('PV loop for left ventricle: exercise');
 xlabel('Volume');
 ylabel('Pressure');
 %systemic arterial pressure-volume ``loop''
 figure(3)
 plot(Vsa_plot,Psa_plot)
-title('PV loop for systemic artery');
+title('PV loop for systemic artery: exercise');
 xlabel('Volume');
 ylabel('Pressure');
 ESP=max(Psa_plot(1200:1500)); %end systolic pressure
