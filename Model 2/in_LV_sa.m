76df5360d39d0329a8f63f64f21c80faf7f44636
valerieporter <49161769+valerieporter@users.noreply.github.com>
2019-05-01 12:22:48 -0700

    Final O2 Model of Diseased Heart
    Ro = 112.561

--- Model 2/in_LV_sa.m
+++ Model 2/in_LV_sa.m
@@ -1,35 +1,36 @@
 %filename: in_LV_sa.m  (initialization for LV_sa)
-T =0.0125/2;    %Duration of heartbeat (minutes)
-TS=0.0050/2;    %Duration of systole   (minutes)
+T =0.0125;    %Duration of heartbeat (minutes)
+TS=0.0050;    %Duration of systole   (minutes)
 tauS=0.0025;  %CLV time constant during systole (minutes)
 tauD=0.0075;  %CLV time constant during diastole (minutes)
-Rs=(17.86+1.9)/2;     %Systemic resistance (mmHg/(liter/minute))
+Rs=(17.86+1.9);     %Systemic resistance (mmHg/(liter/minute))
 %Valve resistances here are not supposed to be realistic,
 %just small enough to be negligible:
 RMi=0.01;     %mitral valve resistance (mmHg/(liter/minute))
 RAo=0.01;     %aortic valve resistance (mmHg/(liter/minute))
-
+ 
 %Oxygen Concentration Coeffcients
 a_O2 = 0.0031; %mLO2/mmHgO2/dL of blood from Henry's Law
 P_O2 = 70; %mmHg O2
 O_2 = a_O2*P_O2; %mL/dL of blood
-M = 0.250*2; %L/min
-
+M = 0.250; %L/min
+Ro = Rs/(O_2-(M/6.031));
+ 
 %The following value of Csa is approximate;
 %needs adjustment to make blood pressure 120/80:
-
+ 
 Csa=0.00175*127/192;  %Systemic arterial compliance (liters/mmHg)
 CLVS=(0.00003*4/3)*5; %Min (systolic)  value of CLV (liters/mmHg)
 CLVD=(0.0146)*1.3;  %Max (diastolic) value of CLV (liters/mmHg)
 Vsad=0.825;   %Systemic arterial volume when Psa=0 (liters)
 %doubled volume remaining after ejection to decrease the ejection fraction
 VLVd=(0.027*4)/0.6;   %Left ventricular volume when PLV=0 (liters)
-PLA=(5*.8)*0.65;        %Left atrial pressure (mmHg) decrease 20%
+PLA=(5*.8);        %Left atrial pressure (mmHg) decrease 20%
 dt=0.01*T;    %Time step duration (minutes)
 %This choice implies 100 timesteps per cardiac cycle.
 klokmax=15*T/dt; %Total number of timesteps 
 %This choice implies simulation of 15 cardiac cycles.
-PLV=(5*0.8);           %Initial value of PLV (mmHg) decrease 20%
+PLV=(5*0.8);                    %Initial value of PLV (mmHg) decrease 20%
 Psa=45.3;                   %Initial value of Psa (mmHg)
 %set initial valve states:
 SMi=(PLA>PLV); %evaluates to 1 if PLA>PLV, 0 otherwise
