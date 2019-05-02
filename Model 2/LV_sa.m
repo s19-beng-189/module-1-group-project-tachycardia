f0ac7dc9a1704d552f0e14327e80e34d8e316b2d
valerieporter <49161769+valerieporter@users.noreply.github.com>
2019-05-01 22:39:06 -0700

    Blood Clot Sudden Code

--- Model 2/LV_sa.m
+++ Model 2/LV_sa.m
@@ -4,7 +4,7 @@ clf       %and figures
 global T TS tauS tauD;
 global Csa Rs RMi RAo dt CHECK PLA;
 in_LV_sa
-for klok=1:klokmax
+for klok=1:1300
   t=klok*dt;
   PLV_old=PLV;
   Psa_old=Psa;
@@ -25,32 +25,52 @@ for klok=1:klokmax
   Qs_plot(klok)=(Psa/(Ro*O_2))+(M/O_2);
   SMi_plot(klok)=SMi;
   SAo_plot(klok)=SAo;
+  Rs_plot(klok) = Rs;
 end
+
+in_LV_sa_new
+for klok=1301:klokmax
+  t=klok*dt;
+  PLV_old=PLV;
+  Psa_old=Psa;
+  CLV_old=CLV;
+  CLV=CV_now(t,CLVS,CLVD);
+  %find self-consistent 
+  %valve states and pressures:
+  set_SMi_SAo
+  %store in arrays for future plotting:
+  t_plot(klok)=t;
+  CLV_plot(klok)=CLV;
+  PLV_plot(klok)=PLV;
+  Psa_plot(klok)=Psa;
+  VLV_plot(klok)=CLV*PLV+VLVd;
+  Vsa_plot(klok)=Csa*Psa+Vsad;
+  QMi_plot(klok)=SMi*(PLA-PLV)/RMi;
+  QAo_plot(klok)=SAo*(PLV-Psa)/RAo;
+  Qs_plot(klok)=(Psa/(Ro*O_2))+(M/O_2);
+  SMi_plot(klok)=SMi;
+  SAo_plot(klok)=SAo;
+  Rs_plot(klok) = Rs;
+end
+
 Ro
 Rs
 max(Qs_plot)
 min(Qs_plot)
 %plot results:
 figure(1)
-subplot(3,1,1), plot(t_plot,CLV_plot)
-title('Left Ventricular Compliance: Blood Clot (Compensated)');
-subplot(3,1,2), plot(t_plot,PLV_plot,t_plot,Psa_plot)
-legend('PLV','Psa');
-title('Left ventricular pressure and systemic arterial pressure: Blood Clot (Compensated)');
-subplot(3,1,3), plot(t_plot,QMi_plot,t_plot,QAo_plot,t_plot,Qs_plot)
-title('computer simulated pulsatile blood flow: Blood Clot (Compensated)')
-legend('Mitral valve Flow', 'Aortic Valve flow','systemic arterial flow')
+subplot(2,1,1),plot(t_plot,PLV_plot,t_plot,Psa_plot)
+legend('PLV','Psa','Location','east');
+title({'Left Ventricular Pressure and Systemic Arterial Pressure:','Blood Clot'});
+subplot(2,1,2),plot(t_plot,Qs_plot)
+title('Pulsatile Blood Flow: Blood Clot')
+legend('Systemic Arterial Flow','Location','southeast')
 %left ventricular pressure-volume loop
 figure(2)
-plot(VLV_plot(1200:1500),PLV_plot(1200:1500))
-title('PV loop for left ventricle: Blood Clot (Compensated)');
+plot(VLV_plot(2500:3000),PLV_plot(2500:3000))
+hold on
+plot(VLV_plot(800:1300),PLV_plot(800:1300))
+title('PV loop for left ventricle: Blood Clot');
 xlabel('Volume');
 ylabel('Pressure');
-%systemic arterial pressure-volume ``loop''
-figure(3)
-plot(Vsa_plot,Psa_plot)
-title('PV loop for systemic artery: Blood Clot (Compensated)');
-xlabel('Volume');
-ylabel('Pressure');
-ESP=max(Psa_plot(1200:1500)); %end systolic pressure
-EDP=min(Psa_plot(1200:1500)); %end diastolic pressure
+hold off
--- Model 2/in_LV_sa_new.m
+++ Model 2/in_LV_sa_new.m
@@ -1,9 +1,9 @@
-%filename: in_LV_sa.m  (initialization for LV_sa)
+%filename: in_LV_sa_new.m  (initialization for LV_sa)
 T =0.0125;    %Duration of heartbeat (minutes)
 TS=0.0050;    %Duration of systole   (minutes)
 tauS=0.0025;  %CLV time constant during systole (minutes)
 tauD=0.0075;  %CLV time constant during diastole (minutes)
-Rs=(17.86+1.9);     %Systemic resistance (mmHg/(liter/minute))
+Rs=(17.86+1.9)*2;     %Systemic resistance (mmHg/(liter/minute))
 %Valve resistances here are not supposed to be realistic,
 %just small enough to be negligible:
 RMi=0.01;     %mitral valve resistance (mmHg/(liter/minute))
@@ -25,33 +25,18 @@ CLVD=(0.0146)*1.3;  %Max (diastolic) value of CLV (liters/mmHg)
 Vsad=0.825;   %Systemic arterial volume when Psa=0 (liters)
 %doubled volume remaining after ejection to decrease the ejection fraction
 VLVd=(0.027*4)/0.6;   %Left ventricular volume when PLV=0 (liters)
-PLA=(5*.8);        %Left atrial pressure (mmHg) decrease 20%
+PLA=(5*.8)*1.35;        %Left atrial pressure (mmHg) decrease 20%
 dt=0.01*T;    %Time step duration (minutes)
 %This choice implies 100 timesteps per cardiac cycle.
 klokmax=30*T/dt; %Total number of timesteps 
 %This choice implies simulation of 15 cardiac cycles.
-PLV=(5*0.8);                    %Initial value of PLV (mmHg) decrease 20%
-Psa=67;                   %Initial value of Psa (mmHg)
+PLV=(5*0.8)*1.35;                    %Initial value of PLV (mmHg) decrease 20%
 
 %set initial valve states:
 SMi=(PLA>PLV); %evaluates to 1 if PLA>PLV, 0 otherwise
 SAo=(PLV>Psa); %evaluates to 1 if PLV>Psa, 0 otherwise
 CLV=CV_now(0,CLVS,CLVD);  %Initial value of CLV (liters/mmHg)
-%Initialize arrays used to store data for plotting:
-%Although the program will work without doing this, 
-%it will run MUCH faster if MATLAB knows in advance 
-%how much space is needed for these arrays.
-  t_plot=zeros(1,klokmax);
-CLV_plot=zeros(1,klokmax);
-PLV_plot=zeros(1,klokmax);
-Psa_plot=zeros(1,klokmax);
-VLV_plot=zeros(1,klokmax);
-Vsa_plot=zeros(1,klokmax);
-QMi_plot=zeros(1,klokmax);
-QAo_plot=zeros(1,klokmax);
- Qs_plot=zeros(1,klokmax);
-SMi_plot=zeros(1,klokmax);
-SAo_plot=zeros(1,klokmax);
+  
 %For self-checking in the function PLV_Psa_new, set CHECK=1                    
 %To skip the self-checking, set CHECK=0
 CHECK=0;
